0824036ba47ce30de735646c2a1559b890766d3d
yichen2022 <yc33387@my.utexas.edu>
2022-10-22 13:22:07 -0400

    One activity

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.calorietracker
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.calorietracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i(javaClass.name, "onCreate")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val inputStream = assets.open("logo.jpg")
+        val bitmap = BitmapFactory.decodeStream(inputStream)
+        binding.logo.setImageBitmap(bitmap)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(javaClass.name, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(javaClass.name, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(javaClass.name, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(javaClass.name, "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(javaClass.name, "onDestroy")
     }
 }
