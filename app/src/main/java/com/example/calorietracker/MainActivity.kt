bc67e02e0042fa7e39dcb38e7c8604b1279c45c4
yichen2022 <yc33387@my.utexas.edu>
2022-10-22 22:40:32 -0400

    start fragment created

--- app/src/main/java/com/example/calorietracker/MainActivity.kt
+++ app/src/main/java/com/example/calorietracker/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.example.calorietracker
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.calorietracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var resultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Log.d(javaClass.simpleName, "result ok")
+            } else {
+                Log.w(javaClass.simpleName, "Bad activity return code ${result.resultCode}")
+            }
+        }
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i(javaClass.name, "onCreate")
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.logo.setOnClickListener {
+            start()
+        }
+    }
 
+    private fun start() {
+        val intent = Intent(this, Start::class.java)
+        resultLauncher.launch(intent)
     }
 
     override fun onStart() {
