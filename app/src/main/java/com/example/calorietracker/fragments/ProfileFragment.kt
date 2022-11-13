f9ca707c07a91ddcd9c8399a78e7f19858dd3451
yichen2022 <yc33387@my.utexas.edu>
2022-11-13 17:52:38 -0500

    Reject negative heights and weights

--- app/src/main/java/com/example/calorietracker/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/calorietracker/fragments/ProfileFragment.kt
@@ -173,10 +173,10 @@ class ProfileFragment : Fragment() {
             else if (!binding.ageInput.text.toString().all { char -> char.isDigit() }) {
                 Snackbar.make(binding.profileLayout, "Invalid value for age", Snackbar.LENGTH_LONG).show()
             }
-            else if (binding.heightInput.text.toString().toDoubleOrNull() == null) {
+            else if (binding.heightInput.text.toString()[0] == '-' || binding.heightInput.text.toString().toDoubleOrNull() == null) {
                 Snackbar.make(binding.profileLayout, "Invalid value for height", Snackbar.LENGTH_LONG).show()
             }
-            else if (binding.weightInput.text.toString().toDoubleOrNull() == null) {
+            else if (binding.weightInput.text.toString()[0] == '-' || binding.weightInput.text.toString().toDoubleOrNull() == null) {
                 Snackbar.make(binding.profileLayout, "Invalid value for weight", Snackbar.LENGTH_LONG).show()
             }
             else {
